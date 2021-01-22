dc3120e1775f941facb10a48a3fc30541d611049
ffiric00 <73295722+ffiric00@users.noreply.github.com>
2021-01-22 08:28:36 +0100

    Update vjezbe1.c

--- vjezbe1.c
+++ vjezbe1.c
@@ -4,19 +4,19 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define MAX 256 
-
+#define MAX_NAME_SIZE 32
+#define MAX_LINE_SIZE 256
 
 typedef struct _student {
-	char name[MAX];
-	char surname[MAX];
+	char name[MAX_NAME_SIZE];
+	char surname[MAX_NAME_SIZE];
 	float score;
 
 }Student;
 
 int countRows(FILE *fp);
 int readData(Student* st, FILE *fp, int studentNumber);
-int printData(Student* st, int studentNumber);
+int printData(Student* st, int studentNumber); 
 
 int main(void) {
 	Student *st;
@@ -24,30 +24,29 @@ int main(void) {
 	int studentNumber = countRows(fp);
 	st = (Student*)malloc(studentNumber * sizeof(Student));
 	readData(st, fp, studentNumber);
-	printData(st, studentNumber);
+	printData(st, studentNumber); 
+	free(st);
 	system("pause");
 	return 0;
 }
 
 int countRows(FILE *fp) {
-	fp = fopen("zadatak1.txt", "r");
-	char characterRead;
-	int rowNumber = 0;
-	characterRead = fgetc(fp);
-	while (characterRead != EOF)
-	{
-		if (characterRead == '\n')
-			rowNumber++;
-		characterRead = fgetc(fp);
-	}
+	fp = fopen("zadatak1.txt", "r"); 
+	if (fp == NULL)
+		return -1;
+	int rowNumber = 0; 
+	char buffer[MAX_LINE_SIZE];
+	while (fgets(buffer, MAX_LINE_SIZE, fp))
+		rowNumber++;
 	fclose(fp);
 	return rowNumber;
 }
 
 int readData(Student* st, FILE *fp, int studentNumber) {
-	fp = fopen("zadatak1.txt", "r");
-	int i;
-	for (i = 0; i < studentNumber; i++)
+	fp = fopen("zadatak1.txt", "r"); 
+	if (fp == NULL)
+		return -1;
+	for (int i = 0; i < studentNumber; i++)
 		fscanf(fp, "%s %s %f", st[i].name, st[i].surname, &st[i].score);
 	fclose(fp);
 	return 0;
@@ -55,14 +54,14 @@ int readData(Student* st, FILE *fp, int studentNumber) {
 
 int printData(Student* st, int studentNumber) {
 	printf("IME\tPREZIME\tBODOVI\tRELATIVNI BROJ BODOVA\n");
-	int i;
 	float maxScore = 0, relativeScore;
-	for (i = 0; i < studentNumber; i++)
+	for (int i = 0; i < studentNumber; i++)
 		if (st[i].score > maxScore)
 			maxScore = st[i].score;
-	for (i = 0; i < studentNumber; i++) {
+	for (int i = 0; i < studentNumber; i++) {
 		relativeScore = st[i].score / maxScore * 100;
 		printf("%s\t%s\t%f\t%f\n", st[i].name, st[i].surname, st[i].score, relativeScore);
 	}
 	return 0;
-}
+} 
+
